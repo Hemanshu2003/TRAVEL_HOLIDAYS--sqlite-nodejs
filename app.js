daab6bae46e494ce9bebf464f64dd41d078f5196
Hemanshu2003 <113788603+Hemanshu2003@users.noreply.github.com>
2023-10-23 07:24:40 +0530

    update page added

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const db = require("./connectDB");
-const tour = require("./routes/tours");
-const cors = require("cors");
+const express = require('express');
+const db = require('./connectDB');
+const tour = require('./routes/tours');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 app.use(express.json());
 
-app.use("/tours", tour);
+app.use('/tours', tour);
+app.get('/:id', (req, res) => {
+  res.sendFile(__dirname + '/public' + '/updateTour.html');
+});
 
 const PORT = process.env.PORT || 3000;
 
